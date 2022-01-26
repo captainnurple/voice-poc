b41b3c4e9374da47df8040d6c6772171df54aee9
Malcolm Murdock <malcolm.murdock@gmail.com>
2022-01-26 15:34:42 -0800

    WIP: Uploading and onboarding recordings

--- functions/onboardRecording.js
+++ functions/onboardRecording.js
@@ -1,4 +1,5 @@
 // validate call comes from transloadit
+const crypto     = require('crypto');
 
 // FUTURE: somehow validate that the payload originated from the same user as the user value in the payload. Like, in theory a user could log in with one account, submit their transloadit payload with the userID of another account, and their recording would go in there instead. Right?
 
