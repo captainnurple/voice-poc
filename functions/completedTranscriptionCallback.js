0333fb5558308605dd550ad8a6e9c3b496dc29d6
Malcolm Murdock <malcolm.murdock@gmail.com>
2022-03-22 14:13:05 -0700

    WIP: Attempting to fix promise chaining for nested fetches

--- functions/completedTranscriptionCallback.js
+++ functions/completedTranscriptionCallback.js
@@ -4,8 +4,8 @@ exports.handler = (event, context) => {
   console.log(context);
 
   return {
-    statusCode: 200,
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({'body' : "hello, World via callback!"})
+    'statusCode': 200,
+    'headers': {'Content-Type': 'application/json'},
+    'body': JSON.stringify({'body' : "hello, World via callback!"})
   }
 }
